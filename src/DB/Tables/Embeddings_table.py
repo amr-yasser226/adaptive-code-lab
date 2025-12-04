f8df22a1d30389fc3d7dd1841cf05ffa0b465e08
Amr Yasser <amr.yasser200.26@gmail.com>
2025-12-04 15:57:59 +0200

    fix(db) Handled the case of more than one file being sumbitted by user per one sumbission.

--- src/DB/Tables/Embeddings_table.py
+++ src/DB/Tables/Embeddings_table.py
@@ -2,12 +2,13 @@ from DB.connection import connect_db
 conn=connect_db()
 c=conn.cursor()
 c.execute('''CREATE TABLE embeddings (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    submission_id INTEGER NOT NULL,
     vector_ref TEXT NOT NULL,
     model_version TEXT,
     dimension INTEGER,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
-    FOREIGN KEY (id) REFERENCES submissions(id) ON DELETE CASCADE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (submission_id) REFERENCES submissions(id) ON DELETE CASCADE
 );
 
 ''')
