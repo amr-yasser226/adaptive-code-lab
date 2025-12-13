dd1a086ee264a54370f08fca804982d72aa9fa67
Amr Yasser <amr.yasser200.26@gmail.com>
2025-12-13 14:56:02 +0200

    fix(assignment): Fix typos and method renaming

--- src/core/entities/assignment.py
+++ src/core/entities/assignment.py
@@ -1,31 +1,31 @@
 class Assignment:
-    def __init__(self, id , course_id , title , describtion , releaseDate, due_date , max_points , is_published, allow_late_submissions,late_submission_penalty , created_at , updated_at):
-        self.__id =id 
-        self.__course_id =course_id
-        self.title =title
-        self.describtion =describtion
-        self.releaseDate = releaseDate
-        self.dueDate = due_date
-        self.maxpoints = max_points
-        self.ispublished = bool(is_published)
-        self.allowlateSubmissions = bool(allow_late_submissions)
-        self.latesubmission_penealty = late_submission_penalty
+    def __init__(self, id, course_id, title, description, release_date, due_date, max_points, is_published, allow_late_submissions, late_submission_penalty, created_at, updated_at):
+        self.__id = id
+        self.__course_id = course_id
+        self.title = title
+        self.description = description
+        self.release_date = release_date
+        self.due_date = due_date
+        self.max_points = max_points
+        self.is_published = bool(is_published)
+        self.allow_late_submissions = bool(allow_late_submissions)
+        self.late_submission_penalty = late_submission_penalty
         self.created_at = created_at
         self.updated_at = updated_at
     def get_id(self):
         return self.__id
     def get_course_id(self):
         return self.__course_id
-    def addtestcase(self, testcase):
+    def add_test_case(self, testcase):
         pass
     def publish(self):
-        self.ispublished = True
-        pass
+        self.is_published = True
+    
     def unpublish(self):
-        self.ispublished = False
-        pass
-    def extend_deadline(self, new_due_date  ):
-        self.dueDate = new_due_date
+        self.is_published = False
+    
+    def extend_deadline(self, new_due_date):
+        self.due_date = new_due_date
         pass
     def get_submissions(self):
         pass
